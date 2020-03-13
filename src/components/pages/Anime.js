224d6f42a7da44fa2dfc7d8512679eff0c0b53df
EraAmate <valeriiaera@gmail.com>
2020-03-13 16:04:51 +0100

    Add headline and search components to anime page

--- src/components/pages/Anime.js
+++ src/components/pages/Anime.js
@@ -2,7 +2,28 @@ import React from 'react';
 import './Anime.css';
 
 function Header() {
-  return <></>;
+  return (
+    <>
+      <div className="animePage">
+        <h3>Find your favourite anime</h3>
+        <input type="text" className="search" placeholder="Naruto?" />
+      </div>
+    </>
+  );
 }
 
 export default Header;
+
+// function App() {
+//  const [searchItem, setSearchItem] = React.useState("");
+//   const handleChange = event => {
+//     setSearchTerm(event.target.value);
+//   };
+//   return (
+//     <div className="App">
+//       <input
+//         type="text"
+//         placeholder="Search"
+//         value={searchTerm}
+//         onChange={handleChange}
+//       />
