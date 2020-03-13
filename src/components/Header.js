d6cdcd7ce529254897a6228d9d2699bbc739d093
EraAmate <valeriiaera@gmail.com>
2020-03-13 12:07:27 +0100

    Add header inclose  logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './Header.css';
+import Ninja from './assets/Ninja.png';
 
 function Header() {
-  return <h1 className="header">Info Ninja</h1>;
+  return (
+    <header className="header">
+      <img className="logo" src={Ninja} alt="Ninja logo" />
+      <h1 className="title">Info Ninja</h1>
+    </header>
+  );
 }
 
 export default Header;
