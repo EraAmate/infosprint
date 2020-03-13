e09335c117f7ae8555c94dca6647450a6d3d7e47
EraAmate <60475379+EraAmate@users.noreply.github.com>
2020-03-13 15:24:55 +0100

    Add welcome page components (#3)
    * Add welcome text
    
    * Add welcome page components
    
    * Create func Button
    
    * Add buttons functionality
    
    * Add network and game pages, expand router dom

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Welcome from './components/pages/Welcome';
 import Anime from './components/pages/Anime';
 import Question from './components/pages/Question';
+import Network from './components/pages/Network';
+import Game from './components/pages/Game';
 
 function App() {
   return (
@@ -22,6 +24,12 @@ function App() {
             </Route>
             <Route path="/question">
               <Question />
+            </Route>
+            <Route exact path="/network">
+              <Network />
+            </Route>
+            <Route exact path="/game">
+              <Game />
             </Route>
           </Switch>
         </main>
--- src/components/pages/Welcome.js
+++ src/components/pages/Welcome.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import './Welcome.css';
+import DirectBtn from '../DirectBtn';
 
-function Welcome() {
-  return <></>;
+// import { Link } from 'react-router-dom';
+
+function Welcome(props) {
+  return (
+    <>
+      <h3>
+        follow the coolest topics all around Japan and anime with info ninja
+      </h3>
+      <div className="welcPage">
+        <DirectBtn className="welcBtn" name="anime list" destination="/anime" />
+        <DirectBtn
+          className="welcBtn"
+          name="questions"
+          destination="/question"
+        />
+        <DirectBtn className="welcBtn" name="network" destination="/network" />
+        <DirectBtn className="welcBtn" name="game" destination="/game" />
+      </div>
+    </>
+  );
 }
 export default Welcome;
