ab3440005e12eee4cb3abb9e4e343792bafdff44
EraAmate <valeriiaera@gmail.com>
2020-03-13 12:36:24 +0100

    Add welcome text

--- src/components/pages/Welcome.js
+++ src/components/pages/Welcome.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './Welcome.css';
 
 function Welcome() {
-  return <></>;
+  return (
+    <>
+      <h3>
+        follow the coolest topics all around Japan and anime with info ninja
+      </h3>
+      <div className="welcPage"></div>
+    </>
+  );
 }
 export default Welcome;
