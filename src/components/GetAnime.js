3a959a1b2d88e8870bb64b4ba3047e97bb478a6f
EraAmate <valeriiaera@gmail.com>
2020-03-15 15:34:12 +0100

    Fetch and render animes

--- src/components/GetAnime.js
+++ src/components/GetAnime.js
@@ -1,45 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
+import './GetAnime.css'
 
 const Animes = () => {
   // const [hasError, setErrors] = useState(false);
-  const [animes, setAnimes] = useState({});
+  const [animes, setAnimes] = useState([])
+
+  async function getAnimes() {
+    const data = await fetch('https://api.jikan.moe/v3/search/anime?q=limit=30')
+    data.json().then(data => {
+      const myAnimes = data.results.map(result => {
+        const myAnime = {}
+        myAnime.title = result.title
+        myAnime.img = result.image_url
+        myAnime.synopsis = result.synopsis
+        return myAnime
+      })
+      setAnimes(myAnimes)
+    })
+    // .catch(err => setErrors(err));
+  }
 
   useEffect(() => {
-    async function getAnimes() {
-      const results = await fetch(
-        'https://api.jikan.moe/v3/search/anime?q=naruto&limit=16'
-      );
-      results.json().then(results => setAnimes(results.results[0].title)); //createAnime - WHY ? doesnt work!
-      // .catch(err => setErrors(err));
-
-      // function createAnime() {
-      //   const myAnime = {};
-      //   myAnime.title = results.results.title;
-      //   myAnime.image = results.results.image_url;
-      //   myAnime.synopsis = results.results.synopsis;
-      //   return myAnime;
-      // }
-    }
-
-    getAnimes();
-  });
+    getAnimes()
+  }, [])
 
   return (
-    <div>
-      <span>{JSON.stringify(animes)}</span>
-      <hr />
-      {/* <span>Has error: {JSON.stringify(hasError)}</span> */}
-    </div>
-  );
-};
-export default Animes;
-
-// function createAnime(res) {
-//   let myAnime = {};
-//   myAnime.title = res.results.title;
-//   myAnime.image = res.results.image_url;
-//   myAnime.synopsis = res.results.synopsis;
-
-//   return myAnime;
-// }
-// console.log(getAnimes);
+    <>
+      <div className="animeList">
+        {animes.map(anime => (
+          <div key={anime.title}>
+            <p key={anime.title}>{anime.title}</p>
+            <img alt={anime.title} key={anime.img} src={anime.img}></img>
+            <p key={anime.synopsis}>{anime.synopsis}</p>
+          </div>
+        ))}
+      </div>
+      {/* <span>Has error: {JSON.stringify(hasE)}</span> */}
+    </>
+  )
+}
+export default Animes
