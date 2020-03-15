a131e1b0902aa53a7fb65af4e857f65f12de9337
EraAmate <valeriiaera@gmail.com>
2020-03-15 08:35:56 +0100

    Try to get an anime object

--- src/components/pages/GetAnime.js
+++ src/components/pages/GetAnime.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 const Animes = () => {
-  const [hasError, setErrors] = useState(false);
+  // const [hasError, setErrors] = useState(false);
   const [animes, setAnimes] = useState({});
 
   useEffect(() => {
@@ -9,10 +9,16 @@ const Animes = () => {
       const results = await fetch(
         'https://api.jikan.moe/v3/search/anime?q=naruto&limit=16'
       );
-      results
-        .json()
-        .then(results => setAnimes(results.results[0].title))
-        .catch(err => setErrors(err));
+      results.json().then(results => setAnimes(results.results[0].title)); //createAnime - WHY ? doesnt work!
+      // .catch(err => setErrors(err));
+
+      // function createAnime() {
+      //   const myAnime = {};
+      //   myAnime.title = results.results.title;
+      //   myAnime.image = results.results.image_url;
+      //   myAnime.synopsis = results.results.synopsis;
+      //   return myAnime;
+      // }
     }
 
     getAnimes();
