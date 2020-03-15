24cb1c9ea1ecb075645c56acf3ebf14b3c9dd139
EraAmate <valeriiaera@gmail.com>
2020-03-15 09:33:04 +0100

    Add inputs form

--- src/components/pages/Question.js
+++ src/components/pages/Question.js
@@ -6,7 +6,18 @@ function Question() {
   return (
     <>
       <div className="questionPage">
-        <Card />
+        <Card>
+          <p className="cardTitle">Add your favourite anime</p>
+          <form className="add-inputs-form">
+            <input type="text" className="input" placeholder=" anime name " />
+            <input type="text" className="input" placeholder=" anime image " />
+            <input
+              type="text"
+              className="input"
+              placeholder=" anime synopsis "
+            />
+          </form>
+        </Card>
       </div>
     </>
   );
