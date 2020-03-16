142423f19c0a0e264be50ba18877ff929b84a2ee
EraAmate <60475379+EraAmate@users.noreply.github.com>
2020-03-16 12:41:19 +0100

    Add buttons to game and network pages (#11)

--- src/components/pages/Game.js
+++ src/components/pages/Game.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from 'react'
+import './Game.css'
+import DirectBtn from '../DirectBtn'
 
 function Game() {
-  return <></>;
+  return (
+    <>
+      <div className="gamePage">
+        <h3>Lets play</h3>
+      </div>
+      <DirectBtn className="btn" name="back to menu" destination="/" />
+    </>
+  )
 }
 
-export default Game;
+export default Game
--- src/components/pages/Network.js
+++ src/components/pages/Network.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from 'react'
+import './Network.css'
+import DirectBtn from '../DirectBtn'
 
 function Network() {
-  return <></>;
+  return (
+    <>
+      <div className="networkPage">
+        <h3>let's chat</h3>
+      </div>
+      <DirectBtn className="btn" name="back to menu" destination="/" />
+    </>
+  )
 }
 
-export default Network;
+export default Network
